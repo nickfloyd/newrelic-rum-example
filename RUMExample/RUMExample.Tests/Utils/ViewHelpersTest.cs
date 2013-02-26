45bb12c5ad80241d4f11c93c4b8d491f7e8df637
Nick Floyd <nickfloyd@newrelic.com>
2013-02-26 09:02:12 -0800

    init commit

---
+++ RUMExample/RUMExample.Tests/Utils/ViewHelpersTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace RUMExample.Tests.Utils
+{
+    [TestClass]
+    public class ViewHelpersTest
+    {
+        [TestMethod]
+        public void validate_ShouldRenderRum_false_when_action_is_in_collection()
+        {
+
+            Assert.IsFalse(RUMExample.Utils.ViewHelpers.ShouldRenderRum("Home", "Index"));
+        }
+
+        [TestMethod]
+        public void validate_ShouldRenderRum_true_when_action_is_in_collection()
+        {
+            Assert.IsTrue(RUMExample.Utils.ViewHelpers.ShouldRenderRum("Home", "Contact"));
+        }
+
+        [TestMethod]
+        public void validate_ShouldRenderRum_true_when_action_is_emptystring()
+        {
+            Assert.IsTrue(RUMExample.Utils.ViewHelpers.ShouldRenderRum("Home", ""));
+        }
+
+        [TestMethod]
+        public void validate_ShouldRenderRum_true_when_controller_is_emptystring()
+        {
+            Assert.IsTrue(RUMExample.Utils.ViewHelpers.ShouldRenderRum("", "Index"));
+        }
+    }
+}
---
+++ RUMExample/RUMExample/Utils/ViewHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace RUMExample.Utils
+{
+    public class ViewHelpers
+    {
+
+        public static bool ShouldRenderRum(string controllerName, string actionName){
+
+            var searchString = controllerName + "/" + actionName;
+
+            // Can use a static string, config, db etc... to store values
+            var pagesToExclude = ConfigurationManager.AppSettings["NewRelic.Rum.ExcludeViews"].Split(new char[] { ',' });
+            
+            bool shouldRender = !Array.Exists
+            (
+                pagesToExclude,
+                delegate(string s) { return s == searchString; }
+            );
+
+            return shouldRender;
+        }
+
+    }
+}
